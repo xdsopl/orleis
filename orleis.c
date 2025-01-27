7c14e375e1ff64bf045cf67fb2dd1faaaae04ba8
Ahmet Inan <xdsopl@gmail.com>
2025-01-27 17:48:41 +0100

    made wrappers for putchar and getchar

--- orleis.c
+++ orleis.c
@@ -8,60 +8,68 @@ Copyright 2025 Ahmet Inan <xdsopl@gmail.com>
 #include <stdlib.h>
 #include <stdint.h>
 
+int putbyte(int byte) {
+	return putchar_unlocked(byte) == EOF;
+}
+
+int getbyte() {
+	return getchar_unlocked();
+}
+
 int encode() {
 	int byte, prev = -1, count = -1;
-	while ((byte = getchar_unlocked()) != EOF) {
+	while ((byte = getbyte()) != EOF) {
 		if (byte == 0 || byte == 255) {
 			if (prev == byte) {
 				if (count < 255) {
 					++count;
 				} else {
-					if (putchar_unlocked(count) == EOF)
+					if (putbyte(count))
 						return 1;
-					if (putchar_unlocked(byte) == EOF)
+					if (putbyte(byte))
 						return 1;
 					count = 0;
 				}
 			} else if (count < 0) {
-				if (putchar_unlocked(byte) == EOF)
+				if (putbyte(byte))
 					return 1;
 				count = 0;
 				prev = byte;
 			} else {
-				if (putchar_unlocked(count) == EOF)
+				if (putbyte(count))
 					return 1;
-				if (putchar_unlocked(byte) == EOF)
+				if (putbyte(byte))
 					return 1;
 				count = 0;
 				prev = byte;
 			}
 		} else {
 			if (count >= 0) {
-				if (putchar_unlocked(count) == EOF)
+				if (putbyte(count))
 					return 1;
 				count = -1;
 				prev = -1;
 			}
-			if (putchar_unlocked(byte) == EOF)
+			if (putbyte(byte))
 				return 1;
 		}
 	}
-	if (count >= 0 && putchar_unlocked(count) == EOF)
+	if (count >= 0 && putbyte(count))
 		return 1;
 	return 0;
 }
 
 int decode() {
 	int byte;
-	while ((byte = getchar_unlocked()) != EOF) {
-		if (putchar_unlocked(byte) == EOF)
+	while ((byte = getbyte()) != EOF) {
+		if (putbyte(byte))
 			return 1;
 		if (byte == 0 || byte == 255) {
-			int count = getchar_unlocked();
+			int count = getbyte();
 			if (count == EOF)
 				return 1;
 			while (count--)
-				if (putchar_unlocked(byte) == EOF)
+				if (putbyte(byte))
 					return 1;
 		}
 	}
